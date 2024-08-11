7ce241aa3c69d1e4af56ade6a3b0df0dab50ab23
vannam25032005 <25.nguyenvannam.11a1@gmail.com>
2024-08-11 23:21:32 +0700

    Delete Welcome.java

--- Welcome.java
+++
@@ -1,7 +0,0 @@
-
-public class Welcome {
-    public static void main(String[] args) {
-        System.out.println("Hello Nguyen Van Nam!");
-        System.out.println("Welcome to the DHKTPM19A!");
-    }
-}
