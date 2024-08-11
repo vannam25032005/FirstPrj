2e27b795a034e9550e1c24d8496ab3edcc9ab9d6
vannam25032005 <25.nguyenvannam.11a1@gmail.com>
2024-08-11 23:25:07 +0700

    Add files via upload

---
+++ FirstPrj/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello Nguyen Van Nam!");
+        System.out.println("Welcome to the DHKTPM19A!");
+        }
+    }
---
+++ FirstPrj/src/Welcome.java
@@ -0,0 +1,7 @@
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Van Nam!");
+        System.out.println("Welcome to the DHKTPM19A!");
+    }
+}
